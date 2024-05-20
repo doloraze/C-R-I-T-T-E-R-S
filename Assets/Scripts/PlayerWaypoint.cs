a71620e80e5da47239d3b6b7cdb741079029093d
Mich 3 <149168822+doloraze@users.noreply.github.com>
2024-05-20 09:09:49 -0400

    editing prologue

--- Assets/Scripts/PlayerWaypoint.cs
+++ Assets/Scripts/PlayerWaypoint.cs
@@ -11,15 +11,6 @@ public class PlayerWaypoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(wayPoints[currentWayPoint].transform.position, transform.position) < .1f)
-        {
-            currentWayPoint++;
-            transform.Rotate(0, 180f, 0);
-            if(currentWayPoint >= wayPoints.Length)
-            {
-                currentWayPoint = 0;
-            }
-        }
 
         transform.position = Vector2.MoveTowards(transform.position, wayPoints[currentWayPoint].transform.position, Time.deltaTime * PlayerSpeed);
     }
